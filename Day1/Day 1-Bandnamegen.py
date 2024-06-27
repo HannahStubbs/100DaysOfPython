7e89ae36222bf3764b06b7d77497ee80caba0f16
Hannah Stubbs <hannahstubbs03@gmail.com>
2024-06-27 20:19:19 +0200

    Changed code

--- Day1/Day 1-Bandnamegen.py
+++ Day1/Day 1-Bandnamegen.py
@@ -1,9 +1,9 @@
 print("Welcome to the Band Name Generator")
 
-city = input("Please enter your favourite city:\n").capitalize()
-pet = input("Now enter the name of you pet:\n").capitalize()
+city = input("Please enter your favourite city:\t").capitalize()
+pet = input("Now enter the name of you pet:\t").capitalize()
 
-name = (city + " " + pet)
+name = city + " " + pet
 
-print("Here is an idea for your band name:\n" + name)
+print("Here is an idea for your band name:\t" + name)
 
