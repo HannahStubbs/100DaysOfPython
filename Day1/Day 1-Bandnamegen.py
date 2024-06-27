9a410365ab1ea8e7890fd87b7fa44c49d765b0c3
Hannah Stubbs <hannahstubbs03@gmail.com>
2024-06-27 20:07:26 +0200

    first commit

---
+++ Day1/Day 1-Bandnamegen.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator")
+
+city = input("Please enter your favourite city:\n").capitalize()
+pet = input("Now enter the name of you pet:\n").capitalize()
+
+name = (city + " " + pet)
+
+print("Here is an idea for your band name:\n" + name)
+
